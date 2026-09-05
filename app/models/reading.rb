85b1b8d92413d22f2fde983b661c2e4bf45be86c
Ana Perry <anabperry@hotmail.com>
2026-09-05 16:28:41 -0300

    alterações da navbar

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -4,6 +4,6 @@ class Reading < ApplicationRecord
   has_many :reading_cards, dependent: :destroy
   has_many :cards, through: :reading_cards
   has_many :messages, dependent: :destroy
-  
-  act_as_chat
+
+  acts_as_chat
 end
