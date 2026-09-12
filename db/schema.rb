272f1647df962ce30c08a7030ebb27d0c502c8c9
Ana Perry <anabperry@hotmail.com>
2026-09-12 14:16:16 -0300

    solid queue install

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_11_004644) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_12_171112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
