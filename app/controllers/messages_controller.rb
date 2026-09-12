3decceeb5c0ead22d744daad276a96f9a8590223
Ana Perry <anabperry@hotmail.com>
2026-09-12 17:36:10 -0300

    reading

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,8 +18,11 @@ class MessagesController < ApplicationController
   def instructions
     "você vai fazer uma análise de leitura de tarot.
     o assunto da leitura é #{@reading.subject},
+    o método de tiragem é #{@reading.style},
+    as cartas devem ser lidas com o contexto de: #{@reading.show.three_cards_labels} seguindo o significado da string do array em ordem crescente,
     as cartas que saíram foram:
     #{@reading.cards.map {|c| "#{c.card} que significa #{c.meaning}"}.join("\n\n")}
-    reponda em português, usando markdown"
+    considere o significado das posições do array de contexto cruzando com o significado das cartas,
+    reponda em português, usando markdown, não usar bulletpoint"
   end
 end
